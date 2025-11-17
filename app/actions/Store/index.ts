c57bbdc16d0ec8b88bdbee529ecdf2013aff4cd0
Daniel Pumayauli <daniel.pumayauli@externos-cl.cencosud.com>
2025-11-17 23:08:06 +0000

    feat: add crud stores api

--- app/actions/Store/index.ts
+++ app/actions/Store/index.ts
@@ -1,14 +1,49 @@
 // Vamos a usar server actions para el CUD (Create, Update, Delete), es decir, para mutaciones. Para el GET usaremos API routes.
 "use server";
 
-export async function createStore(data: FormData) {
-  // TODO: Implementar la lógica para crear una tienda
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export async function createStore(formData: FormData) {
+  const name = formData.get("name") as string;
+  const address = formData.get("address") as string;
+  const phone = formData.get("phone") as string | null;
+  const slug = formData.get("slug") as string;
+
+  await prisma.store.create({
+    data: {
+      name,
+      address,
+      phone,
+      slug,
+    },
+  });
+
+  revalidatePath("/supremo");
 }
 
-export async function editStore(data: FormData) {
-  // TODO: Implementar la lógica para editar una tienda
+export async function updateStore(id: number, formData: FormData) {
+  const name = formData.get("name") as string;
+  const address = formData.get("address") as string;
+  const phone = formData.get("phone") as string | null;
+
+  await prisma.store.update({
+    where: { id },
+    data: {
+      name,
+      address,
+      phone,
+    },
+  });
+
+  revalidatePath("/supremo");
 }
 
-export async function deleteStore(data: FormData) {
-  // TODO: Implementar la lógica para eliminar una tienda
+export async function deleteStore(id: number) {
+  await prisma.store.update({
+    where: { id },
+    data: { deletedAt: new Date() },
+  });
+
+  revalidatePath("/supremo");
 }
---
+++ app/api/stores/route.ts
@@ -0,0 +1,47 @@
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const stores = await prisma.store.findMany({
+    where: {
+      deletedAt: null,
+    },
+    orderBy: { name: "asc" },
+    omit: {
+      deletedAt: true,
+    },
+  });
+  return NextResponse.json(stores);
+}
+
+// LO VOY A QUITAR...
+
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
+//     const { name, address, phone } = body;
+
+//     if (!name || !address) {
+//       return NextResponse.json(
+//         { error: 'Name and address are required' },
+//         { status: 400 }
+//       )
+//     }
+
+//     const store = await prisma.store.create({
+//       data: {
+//         name,
+//         address,
+//         phone,
+//       },
+//     });
+
+//     return NextResponse.json(store, { status: 201 });
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { error: 'Error creating store' },
+//       { status: 500 }
+//     );
+//   }
+// }
