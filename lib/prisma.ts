0bc33cb4eb21de83391875dc38350126094af5f5
kudamonostudio <kudamonostudio.dev@gmail.com>
2025-12-03 11:01:44 -0300

    Fix prisma errors in develop mode

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,12 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + (process.env.NODE_ENV === 'development' ? '?pgbouncer=true&connection_limit=1' : ''),
+      },
+    },
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
