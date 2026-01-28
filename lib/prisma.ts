a86c455d2622fee9e56f3d30595e7ed18560b0a8
kudamonostudio <kudamonostudio.dev@gmail.com>
2026-01-28 19:32:47 -0300

    fix deploy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,7 +8,7 @@ export const prisma =
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
     datasources: {
       db: {
-        url: process.env.DATABASE_URL + (process.env.NODE_ENV === 'development' ? '?pgbouncer=true&connection_limit=1' : ''),
+        url: process.env.DATABASE_URL + '?pgbouncer=true&connection_limit=1&statement_cache_size=0',
       },
     },
   })
