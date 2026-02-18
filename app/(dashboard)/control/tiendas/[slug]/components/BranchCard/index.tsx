28101c3e2604d1ec01b532c30e812380b1673576
kudamonostudio <kudamonostudio.dev@gmail.com>
2026-02-18 14:34:27 -0300

    fix mostrar email en la card sucursal

--- app/(dashboard)/control/tiendas/[slug]/components/BranchCard/index.tsx
+++ app/(dashboard)/control/tiendas/[slug]/components/BranchCard/index.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 
 interface LocationWithCollaborators extends Location {
   collaboratorAssignments: AssignmentWithCollaborator[];
+  profile?: { email: string | null }[];
 }
 
 interface BranchCardProps {
@@ -28,7 +29,10 @@ interface BranchCardProps {
 function index({ branch, store, onEdit }: BranchCardProps) {
   // Filtrar solo colaboradores activos para mostrar en la card
   // const activeProfiles = branch.profiles.filter((p) => p.isActive !== false); TEMPORAL
-  const activeProfiles = branch.collaboratorAssignments.filter((p) => p.isActive !== false);
+  const activeProfiles = branch.collaboratorAssignments.filter(
+    (p) => p.isActive !== false,
+  );
+  const branchEmail = branch.profile?.[0]?.email || "Sin email asociado";
   return (
     <Card
       key={branch.id}
@@ -53,14 +57,13 @@ function index({ branch, store, onEdit }: BranchCardProps) {
         <div className="flex flex-col justify-center">
           <h1 className="font-bold text-lg lg:text-xl">{branch.name}</h1>
           <p className="text-base text-muted-foreground">{branch.address}</p>
-          <p className="text-base text-muted-foreground font-semibold">
-            {branch.phone}
-          </p>
+          <p className="text-base text-muted-foreground">{branch.phone}</p>
+          <p className="text-base text-muted-foreground">{branchEmail}</p>
         </div>
       </div>
       <ShowAvatars
         items={activeProfiles.map((p) => ({
-        // items={branch.collaboratorAssignments.map((p: AssignmentWithCollaborator) => ({ // MIO
+          // items={branch.collaboratorAssignments.map((p: AssignmentWithCollaborator) => ({ // MIO
           id: p.id,
           name: `${p.collaborator.firstName} ${p.collaborator.lastName}`,
           imageUrl: p.collaborator.image ?? "",
